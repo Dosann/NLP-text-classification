d81cf1ac37d9c29d1f32a5fcc704832beb3585bf
Anna Dosova <anndosova@gmail.com>
2023-02-13 19:01:58 +0300

    Added labels on confusion matrix

--- src/model_utils.py
+++ src/model_utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 
 
-def evaluate(y_true, y_pred, print_cm=True):
+def evaluate(y_true, y_pred, print_cm=True, labels=None):
     """
     Evaluate the results of a model using actual and predicted values.
     
@@ -16,12 +16,12 @@ def evaluate(y_true, y_pred, print_cm=True):
     """
     
     cm = confusion_matrix(y_true, y_pred, normalize='true')
-    cmd = ConfusionMatrixDisplay(cm)
+    cmd = ConfusionMatrixDisplay(cm, display_labels=labels)
 
     if print_cm:
         fig, ax = plt.subplots(figsize=(16,16))
         plt.title('Confusion matrix\n', fontsize=20)
-        cmd.plot(ax=ax);
-    
+        cmd.plot(ax=ax, xticks_rotation='vertical')
+        
     return (f1_score(y_true, y_pred, average='macro'),
             accuracy_score(y_true, y_pred))
